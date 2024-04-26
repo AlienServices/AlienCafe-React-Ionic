6ab49485e6191d686b6859401fa7c1cc0dcce7c1
Kale <kaleckh@gmail.com>
2024-04-26 14:29:24 -0600

    bugs should be fixed

--- src/components/MyPosts.tsx
+++ src/components/MyPosts.tsx
@@ -30,7 +30,7 @@ const Content: React.FC = () => {
     const [value, setValue] = useState('<p>here is my values this is for a test</p><p><br></p><p>																																									this should go in the middle</p><p>idk about thiks one </p><p><br></p><p><br></p><p>lets see what happens</p><p><br></p><h1>this is a big header</h1>');
 
    
-    useEffect(() => {
+    useEffect(() => {        
         getMyPosts()
     }, [])
 
@@ -51,7 +51,7 @@ const Content: React.FC = () => {
 
 
     return (
-        <IonPage className='page' >
+        <IonContent className='page' >
             <IonList>
                 {content ? <>   {content?.hello.map((stuff: any, index: number) => {
                     return (
@@ -61,7 +61,7 @@ const Content: React.FC = () => {
                     )
                 })} </> : <><div>You aint got no posts</div></>}
             </IonList>
-        </IonPage>
+        </IonContent>
     );
 }
 
