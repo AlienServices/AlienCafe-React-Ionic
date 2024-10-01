60ded53cabd509ebc9be25e7f563a20d63a50cc7
kaleckh <kaleckh@gmail.com>
2024-10-01 16:53:08 -0600

    search page is added

--- src/components/searchOptions/profiles.tsx
+++ src/components/searchOptions/profiles.tsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect, useRef, useContext } from "react";
 import ReactQuill from "react-quill";
-import { MyContext } from "../providers/postProvider";
+import { MyContext } from "../../providers/postProvider";
 import { useHistory } from "react-router";
 import "react-quill/dist/quill.snow.css";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
 import "../theme/create.css";
 import {
     IonButton,
@@ -29,22 +27,12 @@ import {
     IonImg,
 } from "@ionic/react";
 import { closeOutline } from "ionicons/icons";
-import Quiz from "../subPages/Quiz";
 
 const Search = () => {
     const [editorHtmlTitle, setEditorHtmlTitle] = useState("");
     const [editorHtml, setEditorHtml] = useState("");
-    const [categories, setCategories] = useState([
-        "Aliens",
-        "Vaccines",
-        "Government",
-        "Space",
-        "9/11",
-        "Covid",
-        "Israel",
-    ]);
+
     const titleQuillRef = useRef(null);
-    const [currentCategory, setCurrentCategory] = useState(categories[0]);
     const history = useHistory();
     const {
         posts,
@@ -66,22 +54,9 @@ const Search = () => {
         <IonPage style={{ paddingTop: '20px', padding: "15px" }}>
             <IonContent>
                 <IonInput placeholder="Search Here"></IonInput>
-                <Swiper
-                    modules={[Pagination]}
-                    spaceBetween={50}
-                    slidesPerView={1}
-                    pagination={{ clickable: true }}
-                    onSlideChange={(swiper) =>
-                        setCurrentCategory(categories[swiper.activeIndex])
-                    } // Update category on slide change
-                    style={{ height: "100%" }}
-                >
-                    {categories.map((category, index) => (
-                        <SwiperSlide key={index}>
-                            <Category category={category} />
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
+                <div>
+                    
+                </div>
             </IonContent>
         </IonPage>
     );
